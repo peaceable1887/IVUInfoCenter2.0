1d1e2c05058e65ce7747daac007fddbbb2dcc246
peaceable1887 <78754401+peaceable1887@users.noreply.github.com>
2022-06-16 13:14:48 +0200

    Delete Trash directory

--- Trash/akademieEventsAlleTermine.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-Plugin Name: AKADEMIE / EVENTS (Alle Termine)
-Description: .....
-Version: 1.0.0
-Author: Felix Hansmann
-*/
--- Trash/neueSupportanfrage.style.php
+++
@@ -1,5 +0,0 @@
-<style>
-    /*Supportanfrage: Neue erstellen*/
-
-
-</style>
